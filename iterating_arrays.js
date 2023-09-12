b12d9b3c2f5594cc67e11830199e1e7d4cbcdad0
ralpmike <ralphmike996@gmail.com>
2023-09-12 14:28:04 +0100

    fourth commit

--- iterating_arrays.js
+++ iterating_arrays.js
@@ -5,18 +5,24 @@
 11. Write a function that finds the largest number in an array of numbers using a loop. */
 
 // QUESTION 10
-// let myArray = ['Book', 'Chair', 'Table', 'Plate', 'Spoon']
+let myArray = ['Book', 'Chair', 'Table', 'Plate', 'Spoon']
 
-// for (let i = 0; i < myArray.length; i++) {
-//   // console.log(myArray[i])
-// }
+for (let i = 0; i < myArray.length; i++) {
+  console.log(myArray[i])
+}
 
 // QUESTION 11
 
 function chooseLargestNUm(arr) {
+  let minNum = 0;
+  let maxNum = 0;
  for (let i = 0; i < arr.length; i++){
-    return Math.max(...arr)
+  if (arr[i] >minNum && arr[i] > maxNum){
+    maxNum = arr[i]
+    continue
   }
+  }
+  return `The maximum number is ${maxNum}`;
  
 }
 
